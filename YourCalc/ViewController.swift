76c42e8952feec9b870597a97af45cbdf742b4de
George Saieed <saieed.george0@gmail.com>
2016-07-07 19:07:35 -0400

    Initial Commit

---
+++ YourCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YourCalc
+//
+//  Created by George Saieed on 7/7/16.
+//  Copyright © 2016 George Saieed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
